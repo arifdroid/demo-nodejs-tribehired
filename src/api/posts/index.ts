25adb4c479d61c1263f9045ee0a8bdad0eececb8
arifdroid <arifdroiddeveloper@gmail.com>
2021-01-21 03:34:53 +0800

    check before add middleware

--- src/api/posts/index.ts
+++ src/api/posts/index.ts
@@ -2,5 +2,6 @@ export default (app)=>{
     
     app.get(`/posts`, require('./getAllPosts').default);
     app.get(`/posts/:id`, require('./findPost').default);
+    app.get(`/posts-custom/:id`, require('./findPostCustom').default);
 }
 
