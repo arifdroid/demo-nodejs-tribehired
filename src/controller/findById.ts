3ce0a8eca200a77a2322f4223ede3109d7cd12fb
arifdroid <arifdroiddeveloper@gmail.com>
2021-01-21 03:27:38 +0800

    find by id

--- src/controller/findById.ts
+++ src/controller/findById.ts
@@ -1,20 +1,14 @@
-const findById = async(array_data, id) => {
-
-    // console.log('\n\n find data', array_data[0], "id" ,id)
-
+const findById = async (array_data, id) => {
     let data;
 
     for (var j = 0; j < array_data.length; j++) {
         if (array_data[j]?.id == id) {
 
             data = array_data[j]
-
-            console.log('\n\n find data', data)
-
-            return array_data[j]
+            return data;
         }
     }
     return data;
 }
 
-export {findById};
+export { findById };
